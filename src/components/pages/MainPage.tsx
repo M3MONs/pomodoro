e80b70090a026cd0ddd5610d3b93f8353176b200
M3MONs <alan.skrrrz@gmail.com>
2024-11-16 23:04:44 +0100

    Add YoutubePlayer component to MainPage for video embedding

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,12 +1,14 @@
 import Background from "@/components/atoms/Background/Background";
 import PomodoroTimer from "@/components/molecules/PomodoroTimer/PomodoroTimer";
+import YoutubePlayer from "../molecules/YoutubePlayer/YoutubePlayer";
 
 const MainPage = () => {
   return (
     <>
       <Background />
-      <div className="container mx-auto flex min-h-screen justify-center items-center">
+      <div className="container mx-auto flex min-h-screen justify-evenly items-center flex-wrap">
         <PomodoroTimer />
+        <YoutubePlayer />
       </div>
     </>
   );
