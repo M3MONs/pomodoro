d85aa278ea8d5839efc900c1e8be8706f6ca1d01
M3MONs <alan.skrrrz@gmail.com>
2024-11-16 20:20:43 +0100

    Add z-index to Background component for proper layering

--- src/components/atoms/Background/Background.tsx
+++ src/components/atoms/Background/Background.tsx
@@ -2,7 +2,10 @@ import background from "@/assets/background.jpg";
 
 const Background = () => {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen blur-sm bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${background})` }} />
+    <div
+      className="fixed top-0 left-0 w-screen h-screen blur-sm bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${background})`, zIndex: "-1" }}
+    />
   );
 };
 
