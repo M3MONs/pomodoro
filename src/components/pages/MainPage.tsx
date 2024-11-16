2fb0672781ad79aee4529496cc5bb55cd8337020
M3MONs <alan.skrrrz@gmail.com>
2024-11-16 21:36:49 +0100

    Add PomodoroTimer component to MainPage for enhanced productivity features

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,9 +1,13 @@
 import Background from "@/components/atoms/Background/Background";
+import PomodoroTimer from "@/components/molecules/PomodoroTimer/PomodoroTimer";
 
 const MainPage = () => {
   return (
     <>
       <Background />
+      <div className="container mx-auto flex min-h-screen justify-center items-center">
+        <PomodoroTimer />
+      </div>
     </>
   );
 };
