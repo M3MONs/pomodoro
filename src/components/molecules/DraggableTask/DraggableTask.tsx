af7efb507f23ecd55e13964f60fb0642f1f53a7a
M3MONs <alan.skrrrz@gmail.com>
2024-11-23 21:26:58 +0100

    Add task update functionality to DraggableTask and ToDoList components

--- src/components/molecules/DraggableTask/DraggableTask.tsx
+++ src/components/molecules/DraggableTask/DraggableTask.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { draggableTaskStyle } from "@/components/utils/listUtils";
 import { useDraggable } from "@dnd-kit/core";
 import React from "react";
@@ -9,10 +10,12 @@ interface DraggableTaskProps {
     value: string;
   };
   handleDelete?: (id: string) => void;
+  handleUpdate?: (value: string) => void;
 }
 
-const DraggableTask: React.FC<DraggableTaskProps> = ({ item, handleDelete }) => {
+const DraggableTask: React.FC<DraggableTaskProps> = ({ item, handleDelete, handleUpdate }) => {
   const { id, value } = item;
+  const [taskText, setTaskText] = React.useState(value);
   const [isHovered, setIsHovered] = React.useState(false);
   const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
     id,
@@ -24,19 +27,42 @@ const DraggableTask: React.FC<DraggableTaskProps> = ({ item, handleDelete }) => 
 
   const handleLeave = () => setIsHovered(false);
 
+  const handleUpdateTask = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskText(e.target.value);
+    handleUpdate && handleUpdate(e.target.value);
+  };
+
   const handleDeleteButton = (e: React.MouseEvent) => {
     e.stopPropagation();
     handleDelete && handleDelete(id);
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.currentTarget.blur();
+    }
+  };
+
   return (
     <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      <div className="text-black flex justify-between items-center w-full h-full" onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
-        <span>{value}</span>
-        {isHovered && (
-          <Button className="float-right w-4 h-6 z-50" variant="destructive" size="sm" onClick={handleDeleteButton}>
-            X
-          </Button>
+      <div className="text-black flex justify-between items-center w-full h-full gap-5" onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
+        {isHovered ? (
+          <>
+            <Input
+              className="h-6 border-none shadow-none text-wrap"
+              defaultValue={taskText}
+              onChange={handleUpdateTask}
+              onKeyDown={handleKeyDown}
+              onFocus={(e) => {
+                e.target.select();
+              }}
+            />
+            <Button className="float-right w-4 h-6 z-50" size="sm" onClick={handleDeleteButton}>
+              X
+            </Button>
+          </>
+        ) : (
+          <span className="h-6">{taskText}</span>
         )}
       </div>
     </div>
--- src/components/organisms/ToDoList/ToDoList.tsx
+++ src/components/organisms/ToDoList/ToDoList.tsx
@@ -2,12 +2,18 @@ import DraggableTask from "@/components/molecules/DraggableTask/DraggableTask";
 import DroppableContainer from "@/components/molecules/DroppableContainer/DroppableContainer";
 import ListColumn from "@/components/molecules/ListColumn/ListColumn";
 import { Card, CardContent } from "@/components/ui/card";
-import { addTask, deleteTask, handleDragEnd, initialItems, initialItemsProps } from "@/components/utils/listUtils";
+import { addTask, deleteTask, handleDragEnd, initialItems, initialItemsProps, updateTask } from "@/components/utils/listUtils";
+import { getItems, storeItems } from "@/components/utils/localStorageUtils";
 import { closestCenter, DndContext, MouseSensor, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ToDoList = () => {
-  const [containers, setContainers] = useState<initialItemsProps>(initialItems);
+  const [containers, setContainers] = useState<initialItemsProps>(getItems() || initialItems);
+
+  // Store the initial items in local storage when the component mounts or when the containers change
+  useEffect(() => {
+    storeItems(containers);
+  }, [containers]);
 
   const handleDndDragEnd = (e: any) => {
     handleDragEnd(e, containers, setContainers);
@@ -21,21 +27,25 @@ const ToDoList = () => {
     addTask(container, setContainers);
   };
 
+  const handleUpdateTask = (value: string, id: string, container: string) => {
+    updateTask(value, id, container, setContainers);
+  };
+
   const sensors = useSensors(
     useSensor(PointerSensor, {
       activationConstraint: {
-        distance: 5,
+        distance: 15,
       },
     }),
     useSensor(MouseSensor, {
       activationConstraint: {
-        distance: 5,
+        distance: 15,
       },
     })
   );
 
   return (
-    <Card className="w-full" style={{ backgroundColor: "hsl(240deg 10% 3.9% / 70%)" }}>
+    <Card className="w-full select-none" style={{ backgroundColor: "hsl(240deg 10% 3.9% / 70%)" }}>
       <CardContent className="grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-4 py-5 h-full">
         <DndContext collisionDetection={closestCenter} onDragEnd={handleDndDragEnd} sensors={sensors}>
           {Object.keys(containers).map((container) => (
@@ -45,7 +55,14 @@ const ToDoList = () => {
               <DroppableContainer key={container} id={container} items={containers[container].items}>
                 {/* Draggable Tasks */}
                 {containers[container].items.map((item) => (
-                  <DraggableTask key={item.id} item={item} handleDelete={(id) => handleTaskDelete(id, container)} />
+                  <DraggableTask
+                    key={item.id}
+                    item={item}
+                    handleDelete={(id) => handleTaskDelete(id, container)}
+                    handleUpdate={(value: string) => {
+                      handleUpdateTask(value, item.id, container);
+                    }}
+                  />
                 ))}
               </DroppableContainer>
             </ListColumn>
