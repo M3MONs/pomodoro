0a7a331c258a89af93152589add02b0ad402a132
M3MONs <alan.skrrrz@gmail.com>
2024-11-23 20:42:50 +0100

    Enhance DraggableTask component with delete functionality and hover effect

--- src/components/molecules/DraggableTask/DraggableTask.tsx
+++ src/components/molecules/DraggableTask/DraggableTask.tsx
@@ -1,28 +1,41 @@
+import { Button } from "@/components/ui/button";
+import { draggableTaskStyle } from "@/components/utils/listUtils";
 import { useDraggable } from "@dnd-kit/core";
 import React from "react";
 
 interface DraggableTaskProps {
   id: string;
+  value: string;
+  handleDelete?: (id: string) => void;
 }
 
-const DraggableTask: React.FC<DraggableTaskProps> = ({ id }) => {
+const DraggableTask: React.FC<DraggableTaskProps> = ({ id, value, handleDelete }) => {
+  const [isHovered, setIsHovered] = React.useState(false);
   const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
     id,
   });
 
-  const style = {
-    transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
-    opacity: isDragging ? 0.5 : 1,
-    padding: "8px",
-    border: "1px solid #ccc",
-    marginBottom: "4px",
-    backgroundColor: "#fff",
-    touchAction: 'none',
+  const style = draggableTaskStyle(transform, isDragging);
+
+  const handleEnter = () => setIsHovered(true);
+
+  const handleLeave = () => setIsHovered(false);
+
+  const handleDeleteButton = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    handleDelete && handleDelete(id);
   };
 
   return (
-    <div ref={setNodeRef} style={style} {...listeners} {...attributes} className="text-black">
-      {id}
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <div className="text-black flex justify-between items-center w-full h-full" onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
+        <span>{value}</span>
+        {isHovered && (
+          <Button className="float-right w-4 h-6 z-50" variant="destructive" size="sm" onClick={handleDeleteButton}>
+            X
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
