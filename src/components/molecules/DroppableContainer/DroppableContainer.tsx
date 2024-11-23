672ac69f049b74cab1d0f82f05f19cda87b59745
M3MONs <alan.skrrrz@gmail.com>
2024-11-23 20:43:11 +0100

    Refactor DroppableContainer to accept children and use itemsProps for improved flexibility

--- src/components/molecules/DroppableContainer/DroppableContainer.tsx
+++ src/components/molecules/DroppableContainer/DroppableContainer.tsx
@@ -1,22 +1,21 @@
 import { useDroppable } from "@dnd-kit/core";
 import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import React from "react";
-import DraggableTask from "../DraggableTask/DraggableTask";
+import { itemsProps } from "@/components/utils/listUtils";
 
 interface DroppableContainerProps {
+  children?: React.ReactNode;
   id: string;
-  items: string[];
+  items: itemsProps[];
 }
 
-const DroppableContainer: React.FC<DroppableContainerProps> = ({ id, items }) => {
+const DroppableContainer: React.FC<DroppableContainerProps> = ({ id, items, children }) => {
   const { setNodeRef } = useDroppable({ id });
 
   return (
     <div ref={setNodeRef} className="h-full">
       <SortableContext id={id} items={items} strategy={verticalListSortingStrategy}>
-        {items.map((item) => (
-          <DraggableTask key={item} id={item} />
-        ))}
+        {children}
       </SortableContext>
     </div>
   );
